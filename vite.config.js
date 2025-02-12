65ae2362a67ec47f304495fae35340e22df629f7
carlos <clm0026@alu.medac.es>
2025-02-12 18:38:26 +0100

    tercera

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
+});
