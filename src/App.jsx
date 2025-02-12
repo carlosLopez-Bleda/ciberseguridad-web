3661431fd239a7e02e91216bd2fce2183d0a0e5a
carlos <clm0026@alu.medac.es>
2025-02-12 17:53:20 +0100

    segunda version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 import Hackers from "./pages/Hackers";
 import Historias from "./pages/Historias";
