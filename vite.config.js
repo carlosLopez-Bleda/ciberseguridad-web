dd953f2116ec7c06c594b26fa1f1de546405b42a
carlos <clm0026@alu.medac.es>
2025-02-12 18:42:50 +0100

    PUERTO RENDER

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
